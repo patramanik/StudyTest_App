25e9586f935eca070b3ea0a8db9a83e8b36158ba
patramanik <patramanik409@gmail.com>
2026-01-07 11:57:10 +0530

    change package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.study_test_app"
+    namespace = "com.patratech.study_test"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.study_test_app"
+        applicationId = "com.patratech.study_test"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ lib/config/environment.dart
@@ -0,0 +1,5 @@
+class Environment {
+  static const String appwriteProjectId = '695dcc5c000d93614d71';
+  static const String appwriteProjectName = 'StudyTest';
+  static const String appwritePublicEndpoint = 'https://sgp.cloud.appwrite.io/v1';
+}
